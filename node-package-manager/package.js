fa4d59e4b7a1b0298dc9953288a41a91cafd76eb
dzkdzk <dzdzaakk@gmail.com>
2025-04-30 21:13:09 +0700

    update dependency

--- node-package-manager/package.js
+++ node-package-manager/package.js
@@ -1,5 +1,9 @@
 import { sayHello, sum } from "dz-learn-npm-library";
+import { max, min } from "dz-learn-npm-library/number";
 
 
 console.info(sayHello("Agent S"));
 console.info(sum([10, 10, 10, 10]));
+
+console.info(min(10, 20));
+console.info(max(10, 20));
