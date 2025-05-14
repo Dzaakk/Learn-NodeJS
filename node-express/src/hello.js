650ed0e95097b231d69e7605f4e23a4c3bd3224d
dzkdzk <dzdzaakk@gmail.com>
2025-05-14 22:36:06 +0700

    basic routing

--- node-express/src/hello.js
+++ node-express/src/hello.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
 app.listen(3000, () => {
     console.info("Server started on port 3000");
 }) 
