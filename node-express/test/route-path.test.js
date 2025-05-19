0aea1a8424996e963c65aba5d0af7a9eabc087c9
dzkdzk <dzdzaakk@gmail.com>
2025-05-19 23:02:52 +0700

    fix route path

--- node-express/test/route-path.test.js
+++ node-express/test/route-path.test.js
@@ -7,7 +7,7 @@ app.get('/products/*.json', (req, res) => {
     res.send(req.originalUrl);
 })
 
-app.get('/categories/*(\\d).json', (req, res) => {
+app.get('/categories/*(\\d+).json', (req, res) => {
     res.send(req.originalUrl);
 })
 
@@ -16,8 +16,8 @@ test("Test Route Path", async () => {
     expect(response.text).toBe("/products/dz.json")
 
     response = await request(app).get("/categories/1234.json");
-    expect(response.text).toBe("/products/wrong.json")
+    expect(response.text).toBe("/categories/1234.json")
 
     response = await request(app).get("/categories/wrong.json");
-    expect(response.text).toBe(404)
+    expect(response.status).toBe(404)
 })
