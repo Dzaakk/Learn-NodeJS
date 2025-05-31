31644eeaa43815df5f531f79a5ad86cbf26d463f
dzkdzk <dzdzaakk@gmail.com>
2025-05-31 21:58:25 +0700

    file upload

--- node-express/test/request-body.test.js
+++ node-express/test/request-body.test.js
@@ -1,9 +1,12 @@
 import express from "express";
+import expressFileUpload from "express-fileupload";
 import request from "supertest";
+
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(expressFileUpload())
 
 app.post('/json', (req, res) => {
     const name = req.body.name;
@@ -18,6 +21,23 @@ app.post('/form', (req, res) => {
         hello: `Hello ${name}`
     })
 })
+
+app.post('/file', async (req, res) => {
+    const textFile = req.files.article;
+    await textFile.mv(__dirname + "/upload/" + textFile.name);
+
+    res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`)
+})
+
+test('Test Request File Upload', async () => {
+    const response = await request(app)
+        .post("/file")
+        .set("Contet-Type", "multipart/form-data")
+        .field("name", "Dzak")
+        .attach("article", __dirname + "/example.txt")
+
+    expect(response.text).toBe("Hello Dzak, you uploaded example.txt")
+});
 
 test('Test Request JSON', async () => {
     const response = await request(app)
